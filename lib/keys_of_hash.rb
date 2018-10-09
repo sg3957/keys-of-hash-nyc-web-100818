51dfa00afda577524eba6d3ba4335639040216f5
Sydney Grant <sg3957@nyu.edu>
2018-10-09 19:13:37 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    arguments.each do |argument|
+      argument
+    end
   end
 end
+
+
+# def number_of_greetings(num, *names)
+#   num.times do 
+#     names.each do |name|
+#       puts "Hello, #{name}!"
+#     end
+#   end
+# end
