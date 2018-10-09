8851d6a289a2bc5415c368a90a179b3471daf6b3
Sydney Grant <sg3957@nyu.edu>
2018-10-09 19:46:34 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,28 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
-end
+  def keys_of(*arguments)
+    arr = []
+    # arguments.each do |argument|
+    #   hash.each do |key, value|
+    #   if value == argument 
+    #       arr << key 
+    #     end #end if 
+    #   end #end hash.each 
+    # end #end arguments.each 
+    # arr << arguments
+    self.each do |key, value| 
+      if arguments.include?(value)
+        arr << key
+      end
+    end 
+    arr
+  end # end method name
+end #end class
+
+
+# def number_of_greetings(num, *names)
+#   num.times do 
+#     names.each do |name|
+#       puts "Hello, #{name}!"
+#     end
+#   end
+# end
