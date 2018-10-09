5649e3bb5827be7004627d2202652488f366afad
Sydney Grant <sg3957@nyu.edu>
2018-10-09 19:21:10 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of(hash, *arguments)
-    arguments.map do |key, value|
-      if hash.value == value 
+    hash.map do |key, value|
+      if *arguments.include?(value) 
     end
   end
 end
